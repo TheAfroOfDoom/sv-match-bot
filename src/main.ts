f222e788731f2137c008b141e6925b8aa1c32813
TheAfroOfDoom <tehafroofdoom@gmail.com>
2025-07-08 12:36:22 -0400

    ✨ add google sheets API methods

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+import { getTeamNames } from "./sheets.ts"
 import { authorize } from "./googleAuth.ts"
+import { google } from "googleapis"
 
 const main = async () => {
 	const auth = await authorize()
+	const sheets = google.sheets({ version: "v4", auth })
+
+	const teamNames = await getTeamNames(sheets)
+	console.log(teamNames)
 }
 
 main()
---
+++ src/sheets.ts
@@ -0,0 +1,39 @@
+import { sheets_v4 } from "googleapis"
+
+type Sheets = sheets_v4.Sheets
+
+// https://docs.google.com/spreadsheets/d/1vGJwvRqUSZhF2BnJf5Kf1Rcjzcfuwg0zqVEoW32oNCI/edit?gid=439191577#gid=439191577
+const spreadsheetId = "1vGJwvRqUSZhF2BnJf5Kf1Rcjzcfuwg0zqVEoW32oNCI"
+
+async function getSheetRows(sheets: Sheets, range: string) {
+	const res = await sheets.spreadsheets.values.get({
+		spreadsheetId,
+		range,
+	})
+	const vals = res.data.values
+	if (vals === null || vals === undefined) {
+		throw new Error("Received undefined sheet-row data")
+	}
+	return vals as string[][]
+}
+
+export async function getTeamNames(sheets: Sheets): Promise<string[][]> {
+	const range = "B3:B14"
+	return await getSheetRows(sheets, range)
+}
+
+export async function updateSheetRows(
+	sheets: Sheets,
+	range: string,
+	values: any[][]
+) {
+	const res = await sheets.spreadsheets.values.update({
+		spreadsheetId,
+		range,
+		valueInputOption: "RAW",
+		requestBody: {
+			values,
+		},
+	})
+	return res
+}
