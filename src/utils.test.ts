bbdece06e7efc545efa12a78867ebb7266875260
TheAfroOfDoom <tehafroofdoom@gmail.com>
2025-07-21 13:35:02 -0400

    ✅ add `SuperviveUUID` tests

---
+++ src/utils.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from "vitest"
+
+import { SuperviveUUID } from "./utils.ts"
+
+describe("SuperviveUUID", () => {
+	it("should not error when given an input with dashes", () => {
+		const input = "4d7f5d72-c78b-459f-b550-f0604137dfec"
+		expect(() => new SuperviveUUID(input)).to.not.throw()
+	})
+
+	it("should error when given an input without dashes", () => {
+		const input = "4d7f5d72c78b459fb550f0604137dfec"
+		expect(() => new SuperviveUUID(input)).to.throw("missing dashes")
+	})
+
+	describe("getRaw", () => {
+		it("should return a string without dashes", () => {
+			const input = "4d7f5d72-c78b-459f-b550-f0604137dfec"
+			const uuid = new SuperviveUUID(input)
+			expect(uuid.getRaw()).to.equal("4d7f5d72c78b459fb550f0604137dfec")
+		})
+	})
+
+	describe("getFormatted", () => {
+		it("should return a string with dashes", () => {
+			const input = "4d7f5d72-c78b-459f-b550-f0604137dfec"
+			const uuid = new SuperviveUUID(input)
+			expect(uuid.getFormatted()).to.equal(input)
+		})
+	})
+})
